22a49926c60ec12cd226b7ba5c8694d4f3e72ab9
weitaohu <824784596@qq.com>
2025-05-21 10:44:34 -0700

    Constructor for HTMLmanager

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,8 +1,17 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
+    private Queue<HTMLTag> tags;
 
-
-  
+    // Constructor
+    public HTMLManager(Queue<HTMLTag> html) {
+        if (html == null) {
+            throw new IllegalArgumentException();
+        }
+        tags = new LinkedList<>();
+        for (HTMLTag tag : html) {
+            tags.add(tag);
+        }
+    }    
+    
 }
